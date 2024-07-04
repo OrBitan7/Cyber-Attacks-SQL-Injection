8e2129223567b414eb8c8439dc7b598862500e45
ronyzrihen <ronyzn3@gmail.com>
2024-07-04 17:24:24 +0300

    js inlined in login page

--- routers/unprotectedRouters/sql_router.js
+++ routers/unprotectedRouters/sql_router.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const { login, getProductsList } = require('../../controllers/unprotectedSQL');
 const express = require("express");
 const {join} = require("path");
+
 const unprotectedSqlRouter = new Router();
 unprotectedSqlRouter.use('/', express.static(join(__dirname, '../../public/unprotectedSQL')));
 unprotectedSqlRouter.get('/login', login)
